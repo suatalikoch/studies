720736218753e01e342e1d2675fe793ab1c83b09
Suat Alikoch <suat.alikoch@gmail.com>
2025-08-17 23:14:59 +0300

    + Separated components for consistency across app

---
+++ src/components/layout/index.ts
@@ -0,0 +1,4 @@
+export { default as Footer } from "@/components/layout/footer";
+export { default as HeaderLanding } from "@/components/layout/header-landing";
+export { default as Header } from "@/components/layout/header";
+export { default as Sidebar } from "@/components/layout/sidebar";
---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Badge } from "@/components/ui/badge";
+export { Button } from "@/components/ui/button";
+export { Card } from "@/components/ui/card";
+export { IconButton } from "@/components/ui/icon-button";
+export { Input } from "@/components/ui/input";
+export { Label } from "@/components/ui/label";
+export { default as StatsCard } from "@/components/ui/stats-card";
+export { default as Testimonials } from "@/components/ui/testimonials";
+export { Textarea } from "@/components/ui/textarea";
