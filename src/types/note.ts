f003fd789bc28a3bebc574c3971468bdf45ba72a
Suat Alikoch <suat.alikoch@gmail.com>
2025-08-26 10:43:42 +0300

    + Tasks's ids now are changed to be uuid instead of bigint8

--- src/types/note.ts
+++ src/types/note.ts
@@ -5,13 +5,16 @@ export interface Note {
   subject: string;
   content?: string | null;
   tags?: string[] | null;
-  status: "draft" | "published" | "archived";
-  priority: 1 | 2 | 3 | 4 | 5;
+  status: Status;
+  priority: NotePriority;
   is_favourite: boolean;
   updated_at: string;
   created_at: string;
   color?: string;
 }
+
+export type Status = "draft" | "published" | "archived";
+export type NotePriority = 1 | 2 | 3 | 4 | 5;
 
 export interface NoteCardProps {
   note: Note;
--- src/types/task.ts
+++ src/types/task.ts
@@ -1,28 +1,27 @@
 export interface Task {
-  id: number;
+  id: string;
+  user_id: string;
   title: string;
   description?: string | null;
-  priority: Priority;
+  priority: TaskPriority;
   category: Category;
   due_date?: string | null;
   completed: boolean;
   starred: boolean;
-  created_at: string;
   updated_at: string;
+  created_at: string;
 }
 
 export type FormState = {
   title: string;
   description: string;
-  priority: Priority;
+  priority: TaskPriority;
   category: Category;
   due_date: string;
 };
 
-export type Priority = "low" | "medium" | "high";
-
+export type TaskPriority = "low" | "medium" | "high";
 export type Category = "Work" | "Personal" | "Shopping" | "Other";
-
 export type Filter =
   | "all"
   | "pending"
