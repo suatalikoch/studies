3ac0880f33c331613a0b0ff829c87b104ec7dacd
Suat Alikoch <suat.alikoch@gmail.com>
2025-09-06 11:42:26 +0300

    + Unauthorised redirection logic for assignments & lectures pages

--- src/app/(workspace)/assignments/page.tsx
+++ src/app/(workspace)/assignments/page.tsx
@@ -1,10 +1,18 @@
 import { Metadata } from "next";
 import AssignmentsClient from "./client";
+import { redirect } from "next/navigation";
+import { getAssignments, getCurrentUser } from "@/lib/supabase/crud";
 
 export const metadata: Metadata = {
   title: "Studies | Assignments",
 };
 
-export default function AssignmentsPage() {
-  return <AssignmentsClient />;
+export default async function AssignmentsPage() {
+  const user = await getCurrentUser();
+
+  if (!user) redirect("/login");
+
+  const assignments = await getAssignments(user.id);
+
+  return <AssignmentsClient assignmentsDb={assignments} />;
 }
--- src/app/(workspace)/lectures/page.tsx
+++ src/app/(workspace)/lectures/page.tsx
@@ -1,10 +1,18 @@
 import { Metadata } from "next";
 import LecturesClient from "./client";
+import { getCurrentUser, getLectures } from "@/lib/supabase/crud";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Studies | Lectures",
 };
 
-export default function LecturesPage() {
-  return <LecturesClient />;
+export default async function LecturesPage() {
+  const user = await getCurrentUser();
+
+  if (!user) redirect("/login");
+
+  const lectures = await getLectures(user.id);
+
+  return <LecturesClient lecturesDb={lectures} />;
 }
