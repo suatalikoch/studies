57838881881c18237f3ff8770a7add6b141a54e4
Suat Alikoch <113116287+suatalikoch@users.noreply.github.com>
2025-10-26 09:02:22 +0200

    + Added announcement bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { Footer, HeaderLanding } from "@/components/Layout";
-import { Features, Hero, Preview, Testimonials } from "@/components/UI";
+import {
+  AnnouncementBar,
+  Features,
+  Hero,
+  Preview,
+  Testimonials,
+} from "@/components/UI";
 
 export default function HomePage() {
   return (
     <div className="max-w-[120rem] min-h-screen mx-auto flex flex-col">
+      <AnnouncementBar />
       <HeaderLanding />
       <Hero />
       <Features />
